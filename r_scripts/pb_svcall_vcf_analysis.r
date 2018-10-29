4b397d2ee40f25b761ced96744527be32893535e
Paul Grabowski <grabowsp@gmail.com>
2018-10-29 12:51:17 -0500

    New PacBio SV gen info

--- r_scripts/pb_svcall_vcf_analysis.r
+++ r_scripts/pb_svcall_vcf_analysis.r
@@ -1,6 +1,10 @@
 # Script to look at PacBio SV calling VCF files
 
+# LOAD LIBRARIES #
 
+# LOAD DATA #
+meta_in <- '/home/t4c1/WORK/grabowsk/data/poplar_branches/meta/poplar_branch_meta_v4.0.txt'
+samp_meta <- read.table(meta_in, header = T, stringsAsFactors = F, sep = '\t')
 
 
 ######
@@ -21,6 +25,12 @@ combo_vcf <- combo_vcf_0[-combo_dup_inds,]
 combo_info_list <- strsplit(combo_vcf[ ,8], split = ';')
 combo_sv_vec <- gsub('SVTYPE=', '', unlist(lapply(combo_info_list, function(x) x[1])))
 combo_vcf$type <- combo_sv_vec
+
+combo_vcf_indel <- combo_vcf[grep('DEL|INS', combo_vcf$type), ]
+
+combo_vcf_indel$sv_length <- abs(sapply(combo_vcf_indel[,8],
+  function(x) as.numeric(gsub('SVLEN=', '',
+  unlist(strsplit(x, split = ';'))[3]))))
 
 # functions to process the individual files
 
@@ -107,22 +117,114 @@ indiv_vcf_files <- system(paste('ls ', data_dir, 'ref.P*vcf', sep = ''),
 indiv_nonoverlap_sites <- lapply(indiv_vcf_files, load_to_nonoverlap_df, 
   combo_df = combo_vcf)
 
+raw_vcfs <- lapply(indiv_vcf_files, load_ind_pbnew_vcf)
+tot_num_indels <- unlist(lapply(raw_vcfs, function(x) 
+  length(grep('INS|DEL', x$type))))
+
+num_indiv_indels <- unlist(lapply(indiv_nonoverlap_sites, function(x)
+  length(grep('INS|DEL', x$type))))
+
+lib_names <- gsub('.vcf', '', gsub(paste(data_dir, 'ref.', sep = ''), '', 
+  indiv_vcf_files))
+
+samp_names <- c()
+for(ln in lib_names){
+  meta_ind <- which(samp_meta$lib_name == ln)
+  tmp_name <- samp_meta$branch_name[meta_ind]
+  samp_names <- c(samp_names, tmp_name)
+}
+
+indiv_sv_df <- data.frame(lib = lib_names, samp = samp_names, 
+  tot_indels = tot_num_indels, indels_not_in_combo = num_indiv_indels, 
+  stringsAsFactors = F)
+
+indiv_sv_df$per_not_in_combo <- (indiv_sv_df$indels_not_in_comb / 
+  indiv_sv_df$tot_indels)
+
+num_indiv_50bp_indels <-  unlist(lapply(indiv_NO_50bp_sites, function(x)
+  length(grep('INS|DEL', x$type))))
+
+gen_indel_raw_df <- function(indiv_df){
+  # generate data.frame from raw VCF of insertions and deletions  and 
+  #   include lengths
+  indiv_bnd_inv_inds <- grep('BND|INV', indiv_df$type)
+  indiv_indels <- indiv_df[-indiv_bnd_inv_inds,]
+  indiv_indels$sv_length <- abs(sapply(indiv_indels[,8],
+    function(x) as.numeric(gsub('SVLEN=', '',
+    unlist(strsplit(x, split = ';'))[3]))))
+  return(indiv_indels)
+}
+
+raw_indel_len_vcfs <- lapply(raw_vcfs, gen_indel_raw_df)
+
+raw_50bp_vcfs <- lapply(raw_indel_len_vcfs, 
+  function(x) x[which(x$sv_length >= 50), ])
+
+tot_num_50bp_indels <- unlist(lapply(raw_50bp_vcfs, function(x)
+  length(grep('INS|DEL', x$type))))
+
+indiv_sv_df$tot_indels_50bp <- tot_num_50bp_indels
+indiv_sv_df$indels_50bp_not_in_combo <- num_indiv_50bp_indels
+indiv_sv_df$per_50bp_not_in_combo <- (indiv_sv_df$indels_50bp_not_in_combo /
+  indiv_sv_df$tot_indels_50bp)
+
+indiv_sv_df <- indiv_sv_df[order(indiv_sv_df$samp), ]
+
+indiv_sv_out_file <- paste(data_dir, 'tmp/newPB_indiv_SV.txt', sep = '')
+write.table(indiv_sv_df, file = indiv_sv_out_file, quote = F, sep = '\t',
+  row.names = F, col.names = T)
+
+#######
+# Shared SVs NOT in combo VCF file
 indiv_NO_site_names_all <- unlist(lapply(indiv_nonoverlap_sites, 
   function(x) x$full_name))
 
-table(table(indiv_NO_site_names_all))
+indiv_NO_site_tab <- table(table(indiv_NO_site_names_all))
+indiv_NO_site_tab
 #     1     2     3     4     5     6     7     8     9    10 
 # 12062  2125  1193   734   548   453   339   235   162    58
 
 indiv_NO_50bp_sites <- lapply(indiv_nonoverlap_sites, 
   function(x) x[which(x$sv_length >= 50), ])
+
 indiv_NO_50bp_names_all <- unlist(lapply(indiv_NO_50bp_sites, 
   function(x) x$full_name))
 
-tab_NO50 <- table(indiv_NO_50bp_names_all)
-table(tab_NO50)
+
+tab_NO50 <- table(table(indiv_NO_50bp_names_all))
+tab_NO50
 #    1    2    3    4    5    6    7    8    9   10 
 # 1635  200  100   69   40   35   30   18   11    6 
+
+indiv_NO_tab_df <- data.frame(
+  n_samps_with_SV_missing_in_combo = as.numeric(names(indiv_NO_site_tab)), 
+  all_indels = as.vector(indiv_NO_site_tab), 
+  indels_50bp = as.vector(tab_NO50), stringsAsFactors = F)
+
+indiv_sv_tab_out_file <- paste(data_dir, 'tmp/newPB_indiv_SV_table.txt', 
+  sep = '')
+write.table(indiv_NO_tab_df, file = indiv_sv_tab_out_file, quote = F, 
+  sep = '\t', row.names = F, col.names = T)
+
+sum(indiv_NO_tab_df$all_indels[2:10])
+# 5847
+sum(indiv_NO_tab_df$indels_50bp[2:10])
+# 509
+
+n_indels_in_combo <- nrow(combo_vcf_indel)
+# [1] 56464
+
+n_indels_50bp_in_combo <- sum(combo_vcf_indel$sv_length >= 50)
+# [1] 30665
+
+sum(indiv_NO_tab_df$all_indels[2:10]) / n_indels_in_combo
+# [1] 0.1035527
+
+sum(indiv_NO_tab_df$indels_50bp[2:10]) / n_indels_50bp_in_combo
+# [1] 0.01659873
+
+
+
 
 weird_sites <- names(tab_NO50)[which(tab_NO50 == 10)]
 
